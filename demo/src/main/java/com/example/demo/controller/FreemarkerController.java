5067719f46d7447616ac0ce7e103e48bd10b888f
hanyidong <han673755695@163.com>
2019-05-08 22:39:25 +0800

    添加后台管理模板页面,整理首页

--- demo/src/main/java/com/example/demo/controller/FreemarkerController.java
+++ demo/src/main/java/com/example/demo/controller/FreemarkerController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,14 +23,24 @@ public class FreemarkerController {
 
 	@RequestMapping("/index")
 	public String index(Model model, HttpServletRequest request) {
-		System.out.println("进入freemarker首页");
+		//字符串类型的值
 		model.addAttribute("name", "张三");
+		//list类型的值
 		List list = new ArrayList();
 		list.add(0);
 		list.add(1);
 		list.add(2);
 		model.addAttribute("list", list);
-		return "index";
+		//map类型的值
+		Map<String,Object> dataMap = new HashMap();
+		dataMap.put("key1", "value1");
+		dataMap.put("key2", "value2");
+		dataMap.put("key3", "value3");
+		dataMap.put("key4", "value4");
+		model.addAttribute("dataMap", dataMap);
+		HttpSession session = request.getSession();
+		session.setAttribute("sessionName", "ok");
+		return "/platform/index";
 	}
 	
 	@ResponseBody
